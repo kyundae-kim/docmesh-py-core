59dea7ec5d93bcfee5cad6f7ee1eb94a52c40991
aman <gkd1208@gmail.com>
2026-06-09 18:34:23 +0900

    Remove sample.py as part of project cleanup

--- sample.py
+++
@@ -1,6 +0,0 @@
-from docmesh_py_core import ServiceFactoryRegistry, Settings
-
-settings = Settings()
-registry = ServiceFactoryRegistry(settings=Settings())
-client = registry.create_client("keycloak")
-print(client.check())
